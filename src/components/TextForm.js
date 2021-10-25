819006f33bbdc78602ce11d7fc01d6f0d207efa8
ashutoshkumar <ashukumarrock92471@gmail.com>
2021-10-25 15:08:17 +0530

    word count change

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -79,7 +79,7 @@ export default function TextForm(props) {
         <p>
           Words Count :{" "}
           {
-            text.split(" ").filter((element) => {
+            text.split(/\s+/).filter((element) => {
               return element.length !== 0;
             }).length
           }{" "}
